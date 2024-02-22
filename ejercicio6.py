fc490a2d9e3ff55810c8a9ce897a755456dff0a2
Maikol Alegre <130368640+maikol99@users.noreply.github.com>
2024-02-22 02:32:07 -0300

    Add files via upload

---
+++ ejercicio6.py
@@ -0,0 +1,10 @@
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+
+suma = a + b
+resta= a - b
+multiplicacion = a * b
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Producto:", multiplicacion)
